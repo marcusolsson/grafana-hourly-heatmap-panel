7a042d0e0e394c7ffb664e97d34628997458e866
Marcus Olsson <olsson.e.marcus@gmail.com>
2020-05-27 13:38:10 +0200

    Remove small grouping intervals

--- src/module.ts
+++ src/module.ts
@@ -31,9 +31,6 @@ export const plugin = new PanelPlugin<HeatmapOptions, HeatmapFieldConfig>(Heatma
           name: 'Group by',
           settings: {
             options: [
-              { value: 1, label: '1 minute' },
-              { value: 5, label: '5 minutes' },
-              { value: 10, label: '10 minutes' },
               { value: 15, label: '15 minutes' },
               { value: 30, label: '30 minutes' },
               { value: 60, label: '60 minutes' },
