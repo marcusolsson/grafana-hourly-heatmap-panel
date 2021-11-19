65db208a52688175270ab034bca3fb6a6fa5f8b6
ChrizZz90 <53112257+ChrizZz90@users.noreply.github.com>
2021-11-19 10:48:55 +0100

    Update module.ts (#40)

--- src/module.ts
+++ src/module.ts
@@ -65,6 +65,7 @@ export const plugin = getPanelPluginOrFallback(
                 { value: 15, label: '15 minutes' },
                 { value: 30, label: '30 minutes' },
                 { value: 60, label: '60 minutes' },
+                { value: 120, label: '120 minutes' },
               ],
             },
             defaultValue: 60,
